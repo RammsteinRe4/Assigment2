0c1b05702ff54af238ecaac56fdcfa8879d17afa
RammsteinRe4 <50319253+RammsteinRe4@users.noreply.github.com>
2019-06-10 18:44:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+"""
+Assignment II of Computer Applications for Powers Systems (CAPS) VT-19
+Group 3:
+Héctor Manuel Martínez Álvarez
+Christian Carim Jiménez Saldaña
+
+Some instructions: The default code will classify the learning set (analog_values.csv) with KMean Algorithm, then,
+it will run the algorithm KNN to classify the test set (measurements.csv). The results will be save in a .png images,
+the location of the images is the same as the program folder.
+** Test set can also be classified with KMean Algorithm, see inside comments for details
+"""
 import numpy as np
 import KMeanAlgorithm as kM
-
 
 def extract_data(filePath):
     print('Extractin data from ' + filePath + ' , please wait')
